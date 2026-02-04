0cc391ee8f1caf8641814b03538c9f75f31885cd
miloskrstiic <milos.krstic501@gmail.com>
2026-02-04 17:55:33 +0100

    Izbrisani nepotrebni fajlovi

--- getChargers.js
+++
@@ -1,8 +0,0 @@
-exports.getChargers = async () => {
-  console.log("Getting chargers...");
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ chargers: ["Charger1", "Charger2", "Charger3"] }),
-  };
-};
--- syncOCMdata.js
+++
@@ -1,7 +0,0 @@
-exports.syncOCM = async () => {
-  console.log("Sync OCM data...");
-
-  return {
-    statusCode: 200,
-  };
-};
